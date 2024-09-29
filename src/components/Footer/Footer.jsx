bcf4094b995b10bca33bc9532d6b3a9783a65040
Tarun Kumar <89685711+Tarunrajput04@users.noreply.github.com>
2024-09-30 04:43:46 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,11 @@ const Footer = ({ footerData }) => {
     <footer className="bg-theme pt-7 pb-5 bottom-0 left-0 right-0">
       <div className="nike-container text-slate-200">
         <div className="flex gap-8 items-center justify-center text-slate-200">
-          <a href="https://github.com/JatinSingh08/sneakhead" className="cursor-pointer hover:scale-110 transform duration-300 transition-all" rel="noreferrer"  target="_blank">
+          <a href="https://github.com/Tarunrajput04" className="cursor-pointer hover:scale-110 transform duration-300 transition-all" rel="noreferrer"  target="_blank">
             <BsGithub className="w-auto h-6" />
           </a>
-          <a href="https://www.linkedin.com/in/jatin-kumar-singh-730386204/" className="cursor-pointer hover:scale-110 transform duration-300 transition-all" rel="noreferrer"  target="_blank">
+          <a href="https://www.linkedin.com/in/tarun-kumar-82a124224/" className="cursor-pointer hover:scale-110 transform duration-300 transition-all" rel="noreferrer"  target="_blank">
             <BsLinkedin className="w-auto h-6" />
-          </a>
-          <a href="https://twitter.com/HeyImJatinn" className="cursor-pointer hover:scale-110 transform duration-300 transition-all" rel="noreferrer"  target="_blank">
-            <BsTwitter className="w-auto h-6" />
           </a>
         </div>
         <div className="grid items-start text-start grid-cols-3 max-w-2xl w-full m-auto md:max-w-none md:gap-5 mt-7">
